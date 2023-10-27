f7667845bab5649ca04ebd2d5d10b7701d5b61bb
AndreEva <app.re.app@icloud.com>
2023-10-27 11:37:02 +0300

    working axios

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <script setup>
 import NewArrticle from './components/NewArticle.vue'
 import Articles from './components/Articles.vue'
-
+import axios from 'axios'
 var dataURL = 'https://jsonplaceholder.typicode.com/posts?_limit=20';
 
-let articles = []
+let articles1 = []
 // function mounted(){
 //   // var self = this //?
 //   $.getJSON(dataURL, function (data) {
 //     articles = data;
 //   });
 // }
+
+axios.get('https://jsonplaceholder.typicode.com/posts?_limit=20')
+      .then(res =>{
+        articles1=res;
+        console.log("axios");});
 </script>
 
 <template>
 
   <img class="logo" src="src/assets/logo1.svg" alt="">
+  <p>{{ articles }}</p>
 
   <NewArrticle />
   <h1>Статьи</h1>
-  <Articles />
+  <Articles list-data="articles1"/>
 
 </template>
 
--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,39 +1,131 @@
-<script setup>
+<script>
 
 import axios from 'axios'
 import { reactive, ref } from 'vue'
 import { jsx } from 'vue/jsx-runtime';
 
-// export default {
-//   data(){
-//     return{
-//       articles:[],
-//       size: 3,
-//       pageNumber: 0,
+export default {
+  data(){
+    return{
+      articles:
+      [ { "userId": 1, "id": 1, "title": "test1", "body": "default data" },
+      { "userId": 1, "id": 2, "title": "test2", "body": "default data" }],
+      pageNumber: 0,
 
-//     }
-//   },
-//   methods: {
-//     getArticles(){
-//       // axios.get('https://jsonplaceholder.typicode.com/posts?_limit=20')
+    }
+  },
+  props: {
+    listData: {
+      type: Array,
+      required: true
+    },
+    size: {
+      type: Number,
+      required: false,
+      default: 6
+    }
+  },
+  methods: {
+    getArticles(){
+      axios.get('https://jsonplaceholder.typicode.com/posts?_limit=20')
+      .then(res =>{
+        this.articles=res;
+        console.log("AAAAAA");});
+
+    },
+    nextPage() {
+  this.pageNumber++;
+},
+prevPage() {
+  this.pageNumber--;
+},
+selectPage(page) {
+  this.pageNumber = page;
+},
+pageCount() {
+  let l = this.articles.length,
+    s = this.size;
+  return Math.ceil(l / s);
+},
+paginatedData() {
+  const start = this.pageNumber * this.size,
+    end = start + this.size;
+  // return articles.slice(start, end);
+  return this.articles
+        .slice(start, end);
+        
+},
+
+
+  },
+  computed:{
+
+},
+  mounted(){
+ // getArticles();
+ axios.get('https://jsonplaceholder.typicode.com/posts?_limit=20')
+.then(res =>{
+  this.articles=res.data;
+  console.log("AAAAAA");
+});
+
+ 
+}
+
+}
+
+
+// var dataURL = 'https://jsonplaceholder.typicode.com/posts?_limit=20';
+
+// const size = 3
+// // let pageNumber = 0
+
+// let counter = reactive({
+//   pageNumber: 0
+// })
+
+// let articles = 
+// [ { "userId": 1, "id": 1, "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto" },
+//  { "userId": 1, "id": 2, "title": "qui est esse", "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla" }
+// ]
+
+
+
+// function mounted(){
+//   // var self = this //?
+//   //axios plugin
+
+//   $.getJSON(dataURL, function (data) {
+//     articles = data;
+    
+//   });
+// //  return articles;
+//   // axios.get('https://jsonplaceholder.typicode.com/posts?_limit=20')
 // // .then(res =>(articles));
 
-//     },
-//     nextPage() {
+// }
+// mounted()
+
+
+
+// function nextPage() {
 //   counter.pageNumber++;
-// },
-// prevPage() {
+// }
+// function prevPage() {
 //   counter.pageNumber--;
-// },
-// selectPage(page) {
+// }
+
+// function selectPage(page) {
 //   counter.pageNumber = page;
-// },
-// pageCount() {
+// }
+
+// function pageCount() {
 //   let l = articles.length,
 //     s = size;
 //   return Math.ceil(l / s);
-// },
-// paginatedData() {
+// }
+
+// function paginatedData() {
 //   const start = counter.pageNumber * size,
 //     end = start + size;
 //   // return articles.slice(start, end);
@@ -42,76 +134,13 @@ import { jsx } from 'vue/jsx-runtime';
 //         //nn
 // }
 
-//   },
-
-// }
-
-
-var dataURL = 'https://jsonplaceholder.typicode.com/posts?_limit=20';
-
-const size = 3
-// let pageNumber = 0
-
-let counter = reactive({
-  pageNumber: 0
-})
-
-let articles = 
-[ { "userId": 1, "id": 1, "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto" },
- { "userId": 1, "id": 2, "title": "qui est esse", "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla" }
-]
-
-
-
-function mounted(){
-  // var self = this //?
-  //axios plugin
-
-  $.getJSON(dataURL, function (data) {
-    articles = data;
-    
-  });
-//  return articles;
-  // axios.get('https://jsonplaceholder.typicode.com/posts?_limit=20')
-// .then(res =>(articles));
-
-}
-mounted()
-
-
-
-function nextPage() {
-  counter.pageNumber++;
-}
-function prevPage() {
-  counter.pageNumber--;
-}
-
-function selectPage(page) {
-  counter.pageNumber = page;
-}
-
-function pageCount() {
-  let l = articles.length,
-    s = size;
-  return Math.ceil(l / s);
-}
-
-function paginatedData() {
-  const start = counter.pageNumber * size,
-    end = start + size;
-  // return articles.slice(start, end);
-  return articles
-        .slice(start, end);
-        //nn
-}
-
 
 </script>
 
 
 <template>
   <div>
+    <!-- <p>{{ articles }}</p> -->
 
     <div v-for="p in paginatedData()">
 
@@ -119,31 +148,23 @@ function paginatedData() {
         <h3 class="title">{{ p.title }}</h3>
         <p class="body">{{ p.body }}</p>
       </div>
-    </div>
+    </div> 
 
     <div class="pagination">
 
-      <button class="pagination__button" :disabled="counter.pageNumber === 0" @click="prevPage">
+      <button class="pagination__button" :disabled="pageNumber === 0" @click="prevPage">
         &#60;&#60;
       </button>
 
 
       <div v-for="a in pageCount()" >
-        <button class="pagination__button" @click="selectPage(a)">{{ a }}</button>
+        <button class="pagination__button" @click="selectPage(a-1)">{{ a }}</button>
       </div>
 
-      <button class="pagination__button " :disabled="counter.pageNumber >= pageCount() - 1" @click="nextPage">
+      <button class="pagination__button " :disabled="pageNumber >= pageCount() - 1" @click="nextPage">
         &#62;&#62;
       </button>
-      <!-- <button class="pagination__button" @click="articles.push({
-      title: 'newtitle',
-      body: 'newbody'
-    })"></button> -->
-<!-- <p>counter.pageNumber = {{ counter.pageNumber }}</p>
-<p>{{ articles}}</p>
-<p>pageCount = {{ pageCount() }}</p>
-<p>paginatedData {{ paginatedData()}}</p> -->
-<!-- <p>mounted() {{ mounted() }}</p> -->
+    
 
     </div>
 
